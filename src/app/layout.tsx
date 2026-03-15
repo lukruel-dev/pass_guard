ba3a90e6ed9031e6004b3cade789c459cbb4d1cd
LUCIÀNO KRUEL <lkbenq@gmail.com>
2026-03-15 17:50:14 +0000

    Instalei... Saiu a imagem da torre de pisa como logo, hahahahha. Não tem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata, Viewport} from 'next';
 import './globals.css';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
@@ -33,7 +34,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-        <link rel="apple-touch-icon" href="https://picsum.photos/seed/passguard/192/192" />
+        <link rel="apple-touch-icon" href="https://picsum.photos/seed/security-shield-99/192/192" data-ai-hint="security shield" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
