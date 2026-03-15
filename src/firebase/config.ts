75eb9afdfb8185edcbbaad08f9def47155cf0b74
LUCIÀNO KRUEL <lkbenq@gmail.com>
2026-03-15 03:23:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2601402904-32287",
+  "appId": "1:329626950672:web:170815370b190cccb8be95",
+  "apiKey": "AIzaSyCu4BnkefHMzLp-M_KiwGY5tswYREuQQzo",
+  "authDomain": "studio-2601402904-32287.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "329626950672"
+};
